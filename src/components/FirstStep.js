b17c0f4dd63444824b52896c6956c91ceae84b05
Sebastian <sebastianmosquera86@gmail.com>
2021-03-13 00:21:22 -0300

    second commit

--- src/components/FirstStep.js
+++ src/components/FirstStep.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Form, Button } from 'react-bootstrap';
 
-const FirstStep = () => {
+const FirstStep = (props) => {
+
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      First Step Form
-    </div>
-  )
+    <Form className='input-form' onSubmit={handleSubmit(onSubmit)}>
+      <div className='col-md-6 offset-md-3'>
+        <Form.Group controlId='first_name'>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            type='text'
+            name='first_name'
+            placeholder='Enter your first name'
+            autoComplete='off'
+            ref={register({
+              required: 'First name is required.',
+              pattern: {
+                value: /^[a-zA-Z]+$/,
+                message: 'First name should contain only characters.'
+              }
+            })}
+            className={`${errors.first_name ? 'input-error' : ''}`}
+          />
+          {errors.first_name && (
+            <p className='errorMsg'>{errors.first_name.message}</p>
+          )}
+        </Form.Group>
+
+        <Form.Group controlId='last_name'>
+
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>
+          Next
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default FirstStep;
